97148ff710846ac7aa699f1ffef31ee3c4123b37
Slava Nossar <slava.nossar@gmail.com>
2023-09-16 20:37:50 +1000

    reverse comparison

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ macDisplay.prototype.getServices = function() {
 // Returns proper state of display
 macDisplay.prototype.getSwitchOnCharacteristic = function(next) {
   exec("system_profiler SPDisplaysDataType | grep -m 1 \"Display Asleep\" | awk '{print $2}'", (err, stdout, stderr) => {
-    console.log(stdout, stdout.includes('Yes'))
-    next(null, stdout.includes('Yes'));
+    next(null, !stdout.includes('Yes'));
   });
 }
 
@@ -46,8 +45,7 @@ macDisplay.prototype.setSwitchOnCharacteristic = function(on, next) {
 
   // Check current status
   exec("system_profiler SPDisplaysDataType | grep -m 1 \"Display Asleep\" | awk '{print $2}'", (err, stdout, stderr) => {
-    console.log(stdout, stdout.includes('Yes'), on)
-    if ((stdout.includes('Yes')) !== on) {
+    if ((!stdout.includes('Yes')) !== on) {
       on ? exec('caffeinate -u -t 1') : exec('pmset displaysleepnow');
     }
     next();
